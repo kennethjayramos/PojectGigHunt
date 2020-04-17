7fde6a5433467a044764ee281f4a2576cbd8779d
kennethjayramos <kenneth.jay.ramos@gmail.com>
2020-04-17 13:39:44 +0800

    Change home view to display upcoming gigs

--- GigHunt/GigHunt/Controllers/HomeController.cs
+++ GigHunt/GigHunt/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using GigHunt.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace GigHunt.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _dbContext;
+
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upComingGigs = _dbContext.Gigs.Include(gigs => gigs.Artist).Where(gigs => gigs.DateTime > DateTime.Now);
+
+            return View(upComingGigs);
         }
 
         public ActionResult About()
